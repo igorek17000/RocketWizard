4b9f56e0a5f482782f9ad6f87cf9d7ab49b3b0ed
Brkic365 <79804244+Brkic365@users.noreply.github.com>
2022-02-20 22:13:58 +0100

    Fixed errors

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -127,7 +127,7 @@ function privacyPolicy() {
         <h3>Usage Data</h3>
         <p>Usage Data is collected automatically when using the Service.</p>
         <p>
-          Usage Data may include information such as Your Device's Internet
+          Usage Data may include information such as Your {"Device's"} Internet
           Protocol address (e.g. IP address), browser type, browser version, the
           pages of our Service that You visit, the time and date of Your visit,
           the time spent on those pages, unique device identifiers and other
@@ -175,11 +175,11 @@ function privacyPolicy() {
             <p>
               <strong>To contact You:</strong> To contact You by email,
               telephone calls, SMS, or other equivalent forms of electronic
-              communication, such as a mobile application's push notifications
-              regarding updates or informative communications related to the
-              functionalities, products or contracted services, including the
-              security updates, when necessary or reasonable for their
-              implementation.
+              communication, such as a mobile {"application's"} push
+              notifications regarding updates or informative communications
+              related to the functionalities, products or contracted services,
+              including the security updates, when necessary or reasonable for
+              their implementation.
             </p>
           </li>
           <li>
@@ -275,12 +275,12 @@ function privacyPolicy() {
         <h2>Transfer of Your Personal Data</h2>
         <p>
           Your information, including Personal Data, is processed at the
-          Company's operating offices and in any other places where the parties
-          involved in the processing are located. It means that this information
-          may be transferred to — and maintained on — computers located outside
-          of Your state, province, country or other governmental jurisdiction
-          where the data protection laws may differ than those from Your
-          jurisdiction.
+          {"Company's"} operating offices and in any other places where the
+          parties involved in the processing are located. It means that this
+          information may be transferred to — and maintained on — computers
+          located outside of Your state, province, country or other governmental
+          jurisdiction where the data protection laws may differ than those from
+          Your jurisdiction.
         </p>
         <p>
           Your consent to this Privacy Policy followed by Your submission of
@@ -332,7 +332,7 @@ function privacyPolicy() {
           acceptable means to protect Your Personal Data, We cannot guarantee
           its absolute security.
         </p>
-        <h1>Children's Privacy</h1>
+        <h1>{"Children's Privacy"}</h1>
         <p>
           Our Service does not address anyone under the age of 13. We do not
           knowingly collect personally identifiable information from anyone
@@ -345,15 +345,15 @@ function privacyPolicy() {
         <p>
           If We need to rely on consent as a legal basis for processing Your
           information and Your country requires consent from a parent, We may
-          require Your parent's consent before We collect and use that
+          require Your {"parent's"} consent before We collect and use that
           information.
         </p>
         <h1>Links to Other Websites</h1>
         <p>
           Our Service may contain links to other websites that are not operated
           by Us. If You click on a third party link, You will be directed to
-          that third party's site. We strongly advise You to review the Privacy
-          Policy of every site You visit.
+          that third {"party's"} site. We strongly advise You to review the
+          Privacy Policy of every site You visit.
         </p>
         <p>
           We have no control over and assume no responsibility for the content,
@@ -384,7 +384,7 @@ function privacyPolicy() {
             By visiting this page on our website:{" "}
             <a
               href="https://rocketwizard.com/contact"
-              rel="external nofollow noopener"
+              rel="external nofollow noopener noreferrrer"
               target="_blank"
             >
               https://rocketwizard.com/contact
