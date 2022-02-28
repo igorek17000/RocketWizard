9d56a772cb6e991bdf350dd32ea4458abaa14fdb
Brkic365 <79804244+Brkic365@users.noreply.github.com>
2022-02-28 16:20:08 +0100

    Hopefully fixed

--- components/Chat.js
+++ components/Chat.js
@@ -2,23 +2,6 @@ import React, { useEffect } from "react";
 
 import { useSession } from "next-auth/react";
 
-const withSession = (Component) => (props) => {
-  const session = useSession();
-
-  // if the component has a render property, we are good
-  if (Component.prototype.render) {
-    return <Component session={session} {...props} />;
-  }
-
-  // if the passed component is a function component, there is no need for this wrapper
-  throw new Error(
-    [
-      "You passed a function component, `withSession` is not needed.",
-      "You can `useSession` directly in your component.",
-    ].join("\n")
-  );
-};
-
 export default function Chat() {
   const { data: session } = useSession();
 
