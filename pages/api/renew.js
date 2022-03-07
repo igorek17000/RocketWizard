c0bfcf339f67e40a280e8e7659e5105f63599429
Brkic365 <79804244+Brkic365@users.noreply.github.com>
2022-03-07 22:10:46 +0100

    Simple stuff

--- pages/api/renew.js
+++ pages/api/renew.js
@@ -11,6 +11,8 @@ export default async function handler(req, res) {
     hmac.update(JSON.stringify(req.body, Object.keys(req.body).sort()));
     const signature = hmac.digest("hex");
 
+    console.log("SIGNATURE IS : ", signature);
+
     if (
       payment.payment_status === "confirmed" &&
       signature === req.headers["x-nowpayments-sig"]
