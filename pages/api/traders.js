6fd011dc3ae079d9f067b6dc787c8fafd0d1d772
Brkic365 <79804244+Brkic365@users.noreply.github.com>
2022-03-12 14:26:31 +0100

    ae

--- pages/api/traders.js
+++ pages/api/traders.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
         winrate: trader.winrate,
         yearlyRoi: trader.yearlyRoi,
         comingSoon: trader.comingSoon || false,
-        subscriberCount: trader.subscribers.length || 0,
+        subscriberCount: trader.subscribers ? trader.subscribers.length : 0,
       };
 
       return newTrader;
