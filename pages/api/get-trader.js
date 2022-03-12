5f2a4519d5fde051b901dc6db068ef0b6a957027
Brkic365 <79804244+Brkic365@users.noreply.github.com>
2022-03-12 14:04:26 +0100

    ae

--- pages/api/get-trader.js
+++ pages/api/get-trader.js
@@ -7,7 +7,9 @@ export default async function handler(req, res) {
     const { id } = req.query;
     const trader = await db.collection("traders").findOne({ id });
 
-    trader.subscribers = await trader.subscribers.map((sub, i) => i);
+    trader.subscribers = await trader.subscribers.map((sub, i) => {
+      return { tier: sub.tier };
+    });
 
     return res.json(trader);
   } else {
