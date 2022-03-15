3c83c882e56dd3cca5b17ccdc1cdb5c3ccc9cfd7
Brkic365 <79804244+Brkic365@users.noreply.github.com>
2022-03-15 21:04:32 +0100

    ae

--- pages/api/apiKeys.js
+++ pages/api/apiKeys.js
@@ -48,7 +48,7 @@ export default async function handler(req, res) {
     const { email } = req.query;
     const user = await db.collection("users").findOne({ email });
 
-    const apiKeys = user.apiKeys || [];
+    let apiKeys = user.apiKeys || [];
 
     apiKeys = apiKeys.map((apiKey) => {
       var apiBytes = CryptoJS.AES.decrypt(apiKey.api, key);
