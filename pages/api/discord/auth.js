b0101cb8f7a297eef87658855185fa9ec7a60a55
Brkic365 <79804244+Brkic365@users.noreply.github.com>
2022-05-09 21:51:10 +0200

    re

--- pages/api/discord/auth.js
+++
@@ -1,9 +0,0 @@
-import {discordConfig} from '@config/discord';
-import Discord from '@lib/discord-api';
-
-export default function handler(req, res) {
-  const discord = new Discord(discordConfig());
-
-  res.redirect(discord.authorizeUrl());
-  //res.status(200).json({ url: discord.authorizeUrl() })
-}
