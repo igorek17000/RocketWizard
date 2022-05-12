28e39ee46e914da5f5642626580932fc6a131bed
Antonio Brkic <79804244+Brkic365@users.noreply.github.com>
2022-05-12 16:14:14 +0200

    jebate

--- config/discord.js
+++ config/discord.js
@@ -7,6 +7,6 @@ export const discordConfig = () => {
     redirectUri: "https://www.rocketwizard.io/api/discord/callback",
     botToken:
       "OTczMzIyNDkzOTMyNzYxMTI5.GN_WZg.u5pNSNm7iqL-fJTOxTpkgcb_ndYO1oK9aAVUEs",
-    bypassRoles: ["952251036645601380", "973323339525423164"],
+    bypassRoles: ["952251036645601380", "973323339525423164", "952252516182159440"],
   };
 };
